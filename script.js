92b389ef17c97f6232200c157b40e18c4acde778
cristianorrm-boop <cristianorrm@hotmail.com>
2025-10-27 20:31:11 -0300

    commit do projeto progtarmação do zero

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+
+let circulo = document.querySelector(".circulo")
+let imagemCopo = document.querySelector(".imagem-copo")
+
+function mudarcor(cor){ 
+
+console.log(cor)
+
+circulo.style.backgroundColor = cor
+
+}
+
+
+function trocaimagem(imagem) {
+
+    imagemCopo.src = imagem 
+
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
